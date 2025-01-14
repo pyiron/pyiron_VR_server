4d18917e8f5463b1aabdf50b6cb68c493053c68b
Niklas Siemer <n.siemer@mpie.de>
2025-01-14 12:52:08 +0100

    Add Manager in lib mode

---
+++ vrplugin/ManagerLib.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+from UnityManager import UnityManager
+from Executor import Executor
+import Formatter
+from Structure import Structure
+from EchoServer import EchoServer
+
+
+class Manager:
+    def __init__(self, port=None):
+        unityManager = UnityManager()
+        executor = Executor()
+        structure = Structure()
+        if structure.structure is None:
+            return
+        echoServer = EchoServer(port)
+
+        echoServer.run_server(unityManager, executor, structure)
+
+
